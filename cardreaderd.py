4edbfbb54429e2924fe143407325db3ef042c640
stigok <stig@stigok.com>
2017-10-16 15:49:55 +0200

    Auth in python and simplify

--- cardreaderd.py
+++ cardreaderd.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
-
+import logging
 from subprocess import Popen, PIPE
 import re
+
+logging.basicConfig(level='DEBUG')
+log = logging.getLogger(__name__)
+
+card_id_pattern = re.compile("(0x[a-f0-9]+)", re.IGNORECASE)
+authorized_cards = './card_ids.txt'
+reader_daemon = './barbatos/barbatos'
+#reader_daemon = './test/reader-daemon'
 
 def open_door():
   with Popen(["./open-door"]) as proc:
     proc.wait()
     return proc.returncode == 0
 
-# TODO: Do authentication sequence here
 def auth_card(card_id):
-  with Popen(["./auth-card", card_id], stdout=PIPE) as proc:
-    proc.wait()
-    return proc.returncode == 0
+  with open(authorized_cards, encoding='utf-8') as fd:
+    for i, line in enumerate(fd):
+      if card_id == line.rstrip():
+        return True
+    return False
 
-with Popen(["barbatos/barbatos"], stdout=PIPE) as proc:
+with Popen([reader_daemon], stdout=PIPE) as proc:
   for line in iter(proc.stdout.readline, b''):
-    card_id = re.findall("0x[a-f0-9]+$", line.decode('utf8'), flags=re.I)
-    # On the very first iteration the subprcess outputs
-    # a status message which will not match the regex.
-    # Will have to check length, or do a .match_count() call first
-    if len(card_id) > 0:
-      card_id = card_id[0]
-      print("Card read: %s" % card_id)
+    match = re.search(card_id_pattern, line.decode('utf-8'))
+    if match and match.group():
+      card_id = match.group()
       if auth_card(card_id):
-        open_door():
+        log.info('Card %s is authorized' % card_id)
+        if open_door():
+          log.info('Door opened')
+        else:
+          log.error('Failed to open door')
+      else:
+        log.warning('Card %s is not authorized' % card_id)
 

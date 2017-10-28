80eb272f25255d9e55df3bcabde5ffdece787893
Stig Otnes Kolstad <stig@stigok.com>
2017-10-28 19:31:46 +0200

    Update test scripts

--- test/run-test.sh
+++ test/run-test.sh
@@ -2,4 +2,9 @@
 daemon=doord.py
 project_root="$(realpath $(dirname $(realpath $0))/..)"
 echo "Running $daemon with mock card reads"
-TESTING=true $project_root/$daemon
+
+set -a
+AUTHORIZED_CARDS_URL="file://localhost$project_root/test/test-cards.json"
+OPEN_DOOR_SCRIPT="$project_root/test/open-door-mock"
+READER_DAEMON="$project_root/test/nfcreader-mock"
+$project_root/$daemon
